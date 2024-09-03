218477b1985ec6145ac26a932284ac9931f3139a
WeiLee <z82111712@gmail.com>
2024-09-03 11:39:00 -0700

    review 1071 gcd string

--- Array+String/1071_GreateCommonDivisorofStrings.ts
+++ Array+String/1071_GreateCommonDivisorofStrings.ts
@@ -13,14 +13,6 @@ Input: str1 = "LEET", str2 = "CODE"
 Output: ""
  */
 
-// failed with case string1="ABCDEF" string2="ABC" expect=""
-function gcdOfStrings(str1: string, str2: string): string {
-  let result = str1;
-  if (str1.indexOf(str2) === -1) return "";
-  result = result.replace(str2, "");
-
-  return result;
-}
 
 // solution from leetcode
 // str1 ABCABC -> ABCDEF
@@ -50,34 +42,27 @@ function gcdOfStrings3(str1: string, str2: string): string {
     // no divider if it's not same
     if (str1 + str2 !== str2 + str1) return ""
 
-    // set a and b as both length we will find this common length
-    let a = str1.length
-    let b = str2.length
+    // we will need to find common divider
+    const gcd = getCommonLength(str1.length, str2.length)
 
-    // now we will need to find common divider
-    const gcd = getCommonLength(a, b)
-
-    // now we slice or substring
+    // we slice/substring, if we get gcd = 0 then we slice (0,0) turn empty string
     return str1.slice(0, gcd);
 };
 
 function getCommonLength(num1: number, num2: number): number {
-    // it's easier to understand we set as 0
-    let gcd: number = 0;
     // we can Math min but why not just add up with total
-    const totalLength = num1 + num2;
+    const n = num1 + num2;
 
     // loop from hight to low to find the greatest common divider
-    for (let i = totalLength; i > 0; i--) {
+    for (let i = n; i > 0; i--) {
         const mou1 = num1 % i;
         const mou2 = num2 % i;
 
         // when we find the one divider both num1 and num2
         // we update our gcd and stop loop
         if (mou1 === 0 && mou2 === 0) {
-            gcd = i;
-            break
+            return i
         }
     }
-    return gcd
+    return 0
 }
