4ab4d04fc5be499ee0775aed401079bd9d39d0ad
WeiLee <z82111712@gmail.com>
2024-09-04 13:14:45 -0700

    review 283 move 0s

--- Two_Pointers/283_Move_zeros.ts
+++ Two_Pointers/283_Move_zeros.ts
@@ -13,41 +13,29 @@ Output: [0]
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums: number[]): void {
-  // initial 2 pointers
-  let firstIdx = 0;
-  let secIdx = 1;
-  // find the length of nums
-  const numsLength = nums.length;
+var moveZeroes = function (nums) {
+  // intial our first and second pointer
+  let first = 0;
+  let second = 1;
+  const n = nums.length;
 
-  // while second pointer under length we continue
-  while (secIdx < numsLength) {
-    // find both first and second ele
-    const firstEle = nums[firstIdx];
-    const secEle = nums[secIdx];
-
-    // first case if both 0 we increase second pointer
-    if (firstEle === 0 && secEle === 0) {
-      secIdx++;
-      continue;
+  while (second < n) {
+    const firstNum = nums[first];
+    const secondNum = nums[second];
+    // we will have 4 situation
+    if (firstNum === 0 && secondNum === 0) {
+      // [0,0] both 0 we need to find the second to be non 0s in order to switch
+      second++;
+    } else if (firstNum !== 0 && secondNum === 0) {
+      // [1,0] we move our first pointer to point next
+      first++;
+    } else if (firstNum === 0 && secondNum !== 0) {
+      // [0, 1] we switch to [1, 0]
+      [nums[first], nums[second]] = [secondNum, firstNum];
+    } else {
+      // [1,1] do nothing but increase pointer
+      first++;
+      second++;
     }
-
-    // second case if we have ele in first but 0 in sec
-    // we increase first Idx becase we want 0 in the end [1, 0]
-    if (firstEle !== 0 && secEle === 0) {
-      firstIdx++;
-      continue;
-    }
-
-    // if we have first 0 and second has ele we will switch
-    if (firstEle === 0 && secEle !== 0) {
-      [nums[firstIdx], nums[secIdx]] = [secEle, firstEle];
-    }
-
-    // we ill increase both pointer when
-    // 1. [0, 1] we switch to [1,0]
-    // 2. [1, 2] we dont switch and doesnt meet first two cases
-    firstIdx++;
-    secIdx++;
   }
 };
