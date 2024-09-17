9ead6a3f4c5ad0201cb7c8e0deeaab338feac4fb
WeiLee <z82111712@gmail.com>
2024-09-17 14:04:58 -0700

    Review 1137 tribonacci number

--- DP_1D/1137_nth_tribonacci.ts
+++ DP_1D/1137_nth_tribonacci.ts
@@ -1,41 +1,25 @@
 /**
- * Example 1:
-
-Input: n = 4
-Output: 4
-Explanation:
-T_3 = 0 + 1 + 1 = 2
-T_4 = 1 + 1 + 2 = 4
-Example 2:
-
-Input: n = 25
-Output: 1389537
- */
-
-/**
  * @param {number} n
  * @return {number}
  */
 var tribonacci = function (n) {
-  if (n === 0) return 0;
-  // if (n === 1 || n === 2) return 1;
-  // const arr = new Array(n + 1);
-  // arr[0] = 0;
-  // arr[1] = 1;
-  // arr[2] = 1;
-  // for (let i = 3; i <= n; i++) {
-  //     arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
-  // }
-  // return dp[n]
+  // 0 1 1 2 3 5.......
+  let n0 = 0,
+    n1 = 1,
+    n2 = 1;
+  if (!n) return n0;
 
-  let t0 = 0,
-    t1 = 1,
-    t2 = 1;
+  // we start from 3 because we have initial our 0,1,2 value
+  // we stop at 'n'
   for (let i = 3; i <= n; i++) {
-    let tn = t0 + t1 + t2;
-    t0 = t1;
-    t1 = t2;
-    t2 = tn;
+    // in each iteration we update our n0,n1,n2
+    // based on our new number 0+1+1 = 2 (next num)
+    const newN = n0 + n1 + n2;
+    n0 = n1;
+    n1 = n2;
+    n2 = newN;
   }
-  return t2;
+
+  // finally our n2 will be the latest fi number
+  return n2;
 };
