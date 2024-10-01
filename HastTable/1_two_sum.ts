f72f2f4971978e30dbe9cd186e400ea598145ad1
WeiLee <z82111712@gmail.com>
2024-09-30 17:59:35 -0700

    Update 1 two sum

---
+++ HastTable/1_two_sum.ts
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const diff = target - num;
+
+    if (map.has(diff)) return [map.get(diff), i];
+    map.set(num, i);
+  }
+};
