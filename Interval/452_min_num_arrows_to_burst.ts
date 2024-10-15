7711ba32f129c270b1e83295084ac1135c58c437
WeiLee <z82111712@gmail.com>
2024-10-15 11:37:24 -0700

    Update 452 min of arrows to burst

--- Interval/452_min_num_arrows_to_burst.ts
+++ Interval/452_min_num_arrows_to_burst.ts
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var findMinArrowShots = function (points) {
-  if (points.length === 0) return 0;
-
+  if (points.length == 0) return 0;
+  // sort points by end
   points.sort((a, b) => a[1] - b[1]);
-  // at lease 1 burst happen
-  let count = 1;
-  // initial our end from first interval
-  let end = points[0][1];
 
-  // start from index 1
+  // initial count and start 'end'
+  let count = 1;
+  let currentEnd = points[0][1];
+
+  // iterate thru points from index 1
   for (let i = 1; i < points.length; i++) {
-    const start = points[i][0];
-    // if our current start greater than intiial end we increase our burst
-    // update our newly end
-    if (start > end) {
+    const [nextStart, nextEnd] = points[i];
+    // check if current start greater than end
+    if (nextStart > currentEnd) {
       count++;
-      end = points[i][1];
+      // end with new ned
+      currentEnd = nextEnd;
     }
   }
+  // get count
   return count;
 };
