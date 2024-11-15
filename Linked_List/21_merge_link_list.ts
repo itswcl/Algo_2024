a673266961624e5f8f64ceefb35b9375c7e476b3
WeiLee <z82111712@gmail.com>
2024-11-15 12:52:00 -0800

    Update 21 merge link list

--- Linked_List/21_merge_link_list.ts
+++ Linked_List/21_merge_link_list.ts
@@ -11,31 +11,31 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-  // initial dummy and current point to dummy
   let dummy = new ListNode(0);
-  let current = dummy;
+  let cur = dummy;
 
-  // we will continue loop list1 and 2
   while (list1 && list2) {
-    // if val1 < list2 we append list1 and move list 1 to next
-    if (list1.val < list2.val) {
-      current.next = list1;
+    // continueslly attach smaller node to current
+    if (list1.val <= list2.val) {
+      cur.next = list1;
       list1 = list1.next;
-
-      // if list2 val less we move list2
     } else {
-      current.next = list2;
+      cur.next = list2;
       list2 = list2.next;
     }
 
-    // current will continue move to next when the current assigned
-    current = current.next;
+    // move current to next point
+    cur = cur.next;
   }
 
-  // in the end if any left from 1 or 2 we append to current because at this point current is the end
-  if (list1) current.next = list1;
-  if (list2) current.next = list2;
+  // append whatever left in list1/list2
+  cur.next = list1 || list2;
 
-  // we return dummy next because that's where the start pointed
+  // get the dummy next
   return dummy.next;
 };
+
+/**
+ * O(n) visit each node
+ * O(1) few varialbe
+ */
